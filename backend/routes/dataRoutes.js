4230f6c743f5f3631b4b3fef9a91068a1ed6556b
LucSEILER <lucky200331a@gmail.com>
2024-12-20 13:50:38 +0100

    feat:MF-12 Add the getById functionnality

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const dataController = require('../controllers/dataController');
 
 router.get('/', dataController.getData);
+router.get('/:id', dataController.getDataById);
 
 module.exports = router;
