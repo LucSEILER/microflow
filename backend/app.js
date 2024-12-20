e1d0c78d98874f7babf45bba4a60df68b8027dd7
LucSEILER <lucky200331a@gmail.com>
2024-12-20 12:16:06 +0100

    feat/MF-16: Fix backend to enable and allow cors policy

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const dataRoutes = require('./routes/dataRoutes');
 
 const app = express();
 const PORT = 3000;
 const apiPrefix = '/api/v1';
+
+app.use(cors({
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));  
 
 app.use(express.json());
 
